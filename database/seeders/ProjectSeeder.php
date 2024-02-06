97c5c6f40c2693cb4991002225add5c07ccf6d88
AlbertoMessineo <albevorox@gmail.com>
2024-02-06 23:17:13 +0100

    feat: fakers error

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+use\App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $project = new Project();
+
+        $project->save();
+    }
+}
